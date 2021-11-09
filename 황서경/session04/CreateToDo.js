22b449c4261aef57aa77d6c2a59cbbc0568b8eb7
Su-hwa Yu <65851554+SpeakingPotato@users.noreply.github.com>
2021-11-10 00:58:03 +0900

    4주차 확인완료

---
+++ 황서경/session04/CreateToDo.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function CreateToDo({ date, list, onChange, onCreate }) {
+    return (
+        <div>
+            <input
+                name="list"
+                placeholder="할 일"
+                onChange={onChange}
+                value={list}
+            />&nbsp;
+            <input
+                name="date"
+                placeholder="기한"
+                onChange={onChange}
+                value={date}
+            />&nbsp;
+            <button onClick={onCreate}>등록</button>
+        </div>
+    );
+}
+
+export default React.memo(CreateToDo);
