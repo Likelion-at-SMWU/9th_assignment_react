c18bf1caf4f43cf64b6e51dd8cbedd75df800b3f
Su-hwa Yu <65851554+SpeakingPotato@users.noreply.github.com>
2021-11-09 23:38:18 +0900

    4주차 확인완료

---
+++ 김소은/session04/CreateTodo.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function CreateTodo({ item, date, onChange, onCreate}) {
+    return (
+        <div id="createTodo">
+            <input
+                name="item"
+                placeholder="할 일"
+                onChange={onChange}
+                value={item}
+            />
+            <input
+                name="date"
+                type="date"
+                placeholder="마감일"
+                onChange={onChange}
+                value={date}
+            />
+            <button onClick={onCreate}>등록</button>
+        </div>
+    );
+}
+
+export default React.memo(CreateTodo);  //과제1. 코드 최적화하기(React.memo)
